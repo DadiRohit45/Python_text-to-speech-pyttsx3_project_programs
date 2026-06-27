287bab5598c64f95ef120a5eaf5c60e6256d0b76
Dadi Rohit <157350734+DadiRohit45@users.noreply.github.com>
2026-06-27 18:39:21 +0530

    Fix age condition for cartoon recommendations

--- Talikng_cartoon_recommendations/Talking_cartoon_recommendation.py
+++ Talikng_cartoon_recommendations/Talking_cartoon_recommendation.py
@@ -12,7 +12,7 @@ mood=input()
 speak("what type of cartoons you like basically (action,funny,adventure)")
 type=input()
 speak("Ai Recomendations")
-if(age>=18):
+if(age<=18):
     if(mood=="happy" and type=="action"):
         speak(" you should watch : jakiechan ")
     elif(mood=="sad" and type=="funny"):
