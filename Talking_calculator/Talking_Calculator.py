1ba78d70d17724252539e40f807e44fb6647bc7d
Dadi Rohit <157350734+DadiRohit45@users.noreply.github.com>
2026-06-27 14:41:30 +0530

    Add talking calculator functionality with speech output

---
+++ Talking_calculator/Talking_Calculator.py
@@ -0,0 +1,64 @@
+import pyttsx3
+
+def speak(text):
+    engine=pyttsx3.init() 
+    voices=engine.getProperty('voices')
+    engine.setProperty('volume',1.0)
+    engine.setProperty('rate',160)
+    engine.setProperty('voice',voices[1].id)
+    engine.say(text) 
+    print(text) 
+    engine.runAndWait() 
+
+def speak1(emoji,text):
+    engine=pyttsx3.init() 
+    voices=engine.getProperty('voices')
+    engine.setProperty('volume',1.0)
+    engine.setProperty('rate',160)
+    engine.setProperty('voice',voices[1].id)
+    engine.say(text) 
+    print(emoji,text) 
+    engine.runAndWait() 
+    
+speak1("📟","welcome to our easiest calculator to access") 
+def see(m):
+    speak("do you want to continue , answer in yes or no") 
+    c=input("").lower() 
+    if(c=="yes"): 
+        speak("continue the calculation")
+        d=input()
+        m+=d   
+        return m
+    elif(c=='no'):
+        speak("thank you , for using our calculator for calculation") 
+        return None
+    else:
+        speak("answer in yes or no only")
+        see(m)
+def calculator(s): 
+    while(True):  
+        try:
+            v=str(eval(s))
+            speak("the answer for the given expression: "+v) 
+            result=see(v) 
+            if result is None:
+                break
+        except Exception as e:
+            speak("error found"+str(e))
+            s=""   
+            break
+def main():
+    speak("are you interested to do the mathematical calculations ,answer in yes or no:") 
+    b=input("").lower() 
+    if(b=="yes"): 
+        speak("enter the expression:") 
+        a=input()
+        s=""
+        s+=a
+        calculator(s) 
+    elif(b=='no'):
+        speak("thank you , if you need to access again just run the program")
+    else:
+        speak("answer in yes or no only")
+        main()
+main()
